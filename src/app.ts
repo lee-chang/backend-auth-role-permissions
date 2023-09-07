ee0c87df8c27e782f0d8e9b80ffdc4b1d4106e12
vchangcar <ca.y25@hotmail.com>
2023-09-07 14:36:55 -0500

    optimize cors

--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,37 @@
-import express, { Express, NextFunction } from 'express'
+import express, { Express } from 'express'
 import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
 import router from "./routes";
 import helmet from 'helmet'
+import { corsMiddleware } from './core/middleware/cors.middleware';
 
 import { connect as connectMongoDB } from './config/mongoose.config'
-import { AppError } from './core/utils/http.response.util';
 import { ErrorMiddleware } from './core/middleware/errorHandler.middleware';
 
 const app: Express = express()
 
-app.use(helmet())
+// * Middlewares
 
+app.use(helmet())
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(express.json())
+app.disable('x-powered-by')
 
-
-// * Routes
-app.use(router);
-
-
-// * CORS Config
-const whitelist = ['http://localhost:8080', 'http://localhost:5173']
-
-const corsOptions: cors.CorsOptions = {
-  credentials: true,
-  origin: (origin, callback) => {
-    origin = origin || ''
-
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('NOT ALLOWED BY CORS'))
-    }
-  },
-}
-
-// * Middlewares
-app.options('*', cors(corsOptions))
-app.use(cors())
-app.use(ErrorMiddleware)
-
+app.use(corsMiddleware())
 
 // * Content Type Config
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(express.json({ limit: '50mb' }))
 
 
+// * Routes
+app.use(router);
 
+// * Error Handler
+app.use(ErrorMiddleware)
+
+// * Connect to DB
 connectMongoDB()
 
 
